59ca63bdf15048cd9bd7b60c993b774ae48fda44
computerkeeda <samwodson@gmail.com>
2024-03-11 15:09:48 +0530

    * feat(config.yml): update account and faucet coin values to use 'samf' instead of 'token' and 'stake' * fix(x/junction/keeper/msg_server_process_vrf_dispute.go): remove commented out code and add comments to explain the process of handling VRF disputes

--- x/junction/keeper/msg_server_process_vrf_dispute.go
+++ x/junction/keeper/msg_server_process_vrf_dispute.go
@@ -11,25 +11,7 @@ import (
 	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/status"
 )
-/* 
-ProcessVrfDispute is a function used to handle disputes regarding a Verifiable Random Function (VRF).
-It takes in the context and a message of type MsgProcessVrfDispute, and returns a response of type MsgProcessVrfDisputeResponse and an error.
 
-@Problem:
-When a dispute arises, all tracks of the station need to vote on the dispute. This involves creating a serialized_rc and comparing it with the 
-serialized_rc of the VRF currently in use, obtained from the fetch_vrn function. If the serialized_rc matches, the track votes false, 
-indicating the dispute is not valid. If not, the track votes true, indicating the dispute is valid.
-
-@Solution:
-- First, the function verifies if the station exists.
-- Then it checks if the sender is a track of the station.
-- Next, it verifies if the pod number matches the latest pod number of the station.
-- It also checks if the dispute already exists.
-- After verifying the dispute signatures, it proceeds to count the votes and stores the result.
-- If the dispute result is true (indicating the VRN is invalid), the result is in favor of the verifier, and the creator is removed.
-- Conversely, if the dispute result is false (indicating the VRN is valid), the result is in favor of the creator, and the verifier is removed.
-- Finally, a request for a new VRN Generation is sent to the station.
-*/
 func (k msgServer) ProcessVrfDispute(goCtx context.Context, msg *types.MsgProcessVrfDispute) (*types.MsgProcessVrfDisputeResponse, error) {
 	ctx := sdk.UnwrapSDKContext(goCtx)
 
@@ -96,10 +78,11 @@ func (k msgServer) ProcessVrfDispute(goCtx context.Context, msg *types.MsgProces
 
 	if disputeResult {
 		// Do something when disputeResult is true
-		// remove the 
+		// remove the creator since the dispute is inclined in favor of the verifier
 		// generate a new VRN
 	} else {
 		// Do something when disputeResult is false
+		// remove the verifier since the dispute is inclined in favor of the creator
 		// generate a new VRN
 	}
 
