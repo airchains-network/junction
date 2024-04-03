9e4a78911e673478450a1180864fd1c0f2e8b39b
ComputerKeeda <samwodson@gmail.com>
2024-04-03 18:03:53 +0530

    feat: min stake added

--- cmd/junctiond/cmd/config.go
+++ cmd/junctiond/cmd/config.go
@@ -47,6 +47,7 @@ func initAppConfig() (string, interface{}) {
 	// Optionally allow the chain developer to overwrite the SDK's default
 	// server config.
 	srvCfg := serverconfig.DefaultConfig()
+	srvCfg.MinGasPrices = "0.00025amf"
 	// The SDK's default minimum gas price is set to "" (empty value) inside
 	// app.toml. If left empty by validators, the node will halt on startup.
 	// However, the chain developer can set a default app.toml value for their
