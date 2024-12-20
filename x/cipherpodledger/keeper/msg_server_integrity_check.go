c1c36864d23ee62f400f64c9bc18989c1218e07f
Shubham Sharma <samwodson@gmail.com>
2024-12-20 20:03:26 +0530

    feat: implement Avail and Eigen DA provider logic in IntegrityCheck handler
    - Enhanced the IntegrityCheck handler to support Avail and Eigen DA providers, including unmarshalling and validation of blob references.
    - Updated pod data status to "finalized" for specified pod ranges, ensuring accurate tracking of pod states.
    - Improved error handling for invalid blob references and missing pod data, enhancing robustness.
    - Updated FHVM metadata and finalized pod count in the database, ensuring consistency across operations.
    - Refactored code for better organization and maintainability within the Cipherpodledger module.

--- x/cipherpodledger/keeper/msg_server_integrity_check.go
+++ x/cipherpodledger/keeper/msg_server_integrity_check.go
@@ -82,7 +82,7 @@ func (k msgServer) IntegrityCheck(goCtx context.Context, msg *types.MsgIntegrity
 
 				updatePodData := types.PodData{
 					AscContractAddress: podData.AscContractAddress,
-					PodNumber:          pod,
+					PodNumber:          podData.PodNumber,
 					DaBlobId:           podData.DaBlobId,
 					SubmittedBy:        podData.SubmittedBy,
 					Status:             "finalized",
@@ -135,17 +135,161 @@ func (k msgServer) IntegrityCheck(goCtx context.Context, msg *types.MsgIntegrity
 			return nil, status.Error(codes.InvalidArgument, "invalid blobRef passed")
 		}
 	} else if daProvider == "avail" {
+		var blobSpace AvailDABlobSpace
+		err := json.Unmarshal(blobSpaceStoreDataBytes, &blobSpace)
+		if err != nil {
+			return nil, status.Error(codes.InvalidArgument, "failed to unmarshal Avail DABlobSpace")
+		}
 
-		
-		
-		return &types.MsgIntegrityCheckResponse{
-			Status: true,
-		}, nil
+		result := bytes.Equal(blobRef, blobSpace.Commitment)
+
+		if result {
+			upperBondPod := blobSpace.podRange[1]
+			lowerBondPod := blobSpace.podRange[0]
+
+			podStorePath := k.GetPodKeyByteForStation(stationId)
+			podStore := prefix.NewStore(storeAdapter, types.KeyPrefix(podStorePath))
+
+			for pod := lowerBondPod; pod <= upperBondPod; pod++ {
+				key := []byte(fmt.Sprintf("%d", pod))
+				podDetailsBytes := podStore.Get(key)
+				if podDetailsBytes == nil {
+					return nil, status.Error(codes.NotFound, "pod not found")
+				}
+				var podData types.PodData
+				k.cdc.MustUnmarshal(podDetailsBytes, &podData)
+
+				updatePodData := types.PodData{
+					AscContractAddress: podData.AscContractAddress,
+					PodNumber:          podData.PodNumber,
+					DaBlobId:           podData.DaBlobId,
+					SubmittedBy:        podData.SubmittedBy,
+					Status:             "finalized",
+					Timestamp:          podData.Timestamp,
+					ProvingNetwork:     podData.ProvingNetwork,
+					ZkFHEProof:         podData.ZkFHEProof,
+					ZkFHEPublicWitness: podData.ZkFHEPublicWitness,
+					IsProofVerified:    podData.IsProofVerified,
+				}
+
+				storingData := k.cdc.MustMarshal(&updatePodData)
+				podStore.Set(key, storingData)
+			}
+
+			// update the fhvm metadata
+			updateFhvmMetadata := types.FhvmsMeta{
+				ChainId:                       fhvmMetadata.ChainId,
+				ChainName:                     fhvmMetadata.ChainName,
+				Status:                        fhvmMetadata.Status,
+				ProofType:                     fhvmMetadata.ProofType,
+				ProvingNetworkVerificationKey: fhvmMetadata.ProvingNetworkVerificationKey,
+				DaProvider:                    fhvmMetadata.DaProvider,
+				DaBlobId:                      fhvmMetadata.DaBlobId,
+				RelayerGAddress:               fhvmMetadata.RelayerGAddress,
+				RelayerAscAddress:             fhvmMetadata.RelayerAscAddress,
+				PicContractAddress:            fhvmMetadata.PicContractAddress,
+				AclContractAddress:            fhvmMetadata.AclContractAddress,
+				TfheExecutorContractAddress:   fhvmMetadata.TfheExecutorContractAddress,
+				KmsVerifierContractAddress:    fhvmMetadata.KmsVerifierContractAddress,
+				GatewayContractAddress:        fhvmMetadata.GatewayContractAddress,
+				AscChildContractAddress:       fhvmMetadata.AscChildContractAddress,
+				LatestPodNumber:               fhvmMetadata.LatestPodNumber,
+				LatestVerifiedPodNumber:       fhvmMetadata.LatestVerifiedPodNumber,
+				FinalityPodNumber:             upperBondPod,
+			}
+
+			byteFhvmMetadata := k.cdc.MustMarshal(&updateFhvmMetadata)
+			fhvmMetadataDB.Set(fhvmMetadataKey, byteFhvmMetadata)
+			// update the pod-finalized-count
+			figureDBStore := prefix.NewStore(storeAdapter, types.KeyPrefix(types.FiguresDBPath))
+			podFinalizedCountKey := fmt.Sprintf("zkFHE-pod-finalized-count__%s", stationId)
+			podNumberString := strconv.FormatUint(upperBondPod, 10)
+			figureDBStore.Set([]byte(podFinalizedCountKey), []byte(podNumberString))
+
+			return &types.MsgIntegrityCheckResponse{
+				Status: true,
+			}, nil
+		} else {
+			return nil, status.Error(codes.InvalidArgument, "invalid blobRef passed")
+		}
+
 	} else if daProvider == "eigen" {
-		// TODO: Implement the logic for Eigen
-		return &types.MsgIntegrityCheckResponse{
-			Status: true,
-		}, nil
+		var blobSpace EigenDABlobSpace
+		err := json.Unmarshal(blobSpaceStoreDataBytes, &blobSpace)
+		if err != nil {
+			return nil, status.Error(codes.InvalidArgument, "failed to unmarshal Eigen DABlobSpace")
+		}
+
+		result := bytes.Equal(blobRef, blobSpace.Commitment)
+		if result {
+			upperBondPod := blobSpace.podRange[1]
+			lowerBondPod := blobSpace.podRange[0]
+
+			podStorePath := k.GetPodKeyByteForStation(stationId)
+			podStore := prefix.NewStore(storeAdapter, types.KeyPrefix(podStorePath))
+
+			for pod := lowerBondPod; pod <= upperBondPod; pod++ {
+				key := []byte(fmt.Sprintf("%d", pod))
+				podDetailsBytes := podStore.Get(key)
+				if podDetailsBytes == nil {
+					return nil, status.Error(codes.NotFound, "pod not found")
+				}
+				var podData types.PodData
+				k.cdc.MustUnmarshal(podDetailsBytes, &podData)
+
+				updatePodData := types.PodData{
+					AscContractAddress: podData.AscContractAddress,
+					PodNumber:          podData.PodNumber,
+					DaBlobId:           podData.DaBlobId,
+					SubmittedBy:        podData.SubmittedBy,
+					Status:             "finalized",
+					Timestamp:          podData.Timestamp,
+					ProvingNetwork:     podData.ProvingNetwork,
+					ZkFHEProof:         podData.ZkFHEProof,
+					ZkFHEPublicWitness: podData.ZkFHEPublicWitness,
+					IsProofVerified:    podData.IsProofVerified,
+				}
+
+				storingData := k.cdc.MustMarshal(&updatePodData)
+				podStore.Set(key, storingData)
+			}
+
+			// update the fhvm metadata
+			updateFhvmMetadata := types.FhvmsMeta{
+				ChainId:                       fhvmMetadata.ChainId,
+				ChainName:                     fhvmMetadata.ChainName,
+				Status:                        fhvmMetadata.Status,
+				ProofType:                     fhvmMetadata.ProofType,
+				ProvingNetworkVerificationKey: fhvmMetadata.ProvingNetworkVerificationKey,
+				DaProvider:                    fhvmMetadata.DaProvider,
+				DaBlobId:                      fhvmMetadata.DaBlobId,
+				RelayerGAddress:               fhvmMetadata.RelayerGAddress,
+				RelayerAscAddress:             fhvmMetadata.RelayerAscAddress,
+				PicContractAddress:            fhvmMetadata.PicContractAddress,
+				AclContractAddress:            fhvmMetadata.AclContractAddress,
+				TfheExecutorContractAddress:   fhvmMetadata.TfheExecutorContractAddress,
+				KmsVerifierContractAddress:    fhvmMetadata.KmsVerifierContractAddress,
+				GatewayContractAddress:        fhvmMetadata.GatewayContractAddress,
+				AscChildContractAddress:       fhvmMetadata.AscChildContractAddress,
+				LatestPodNumber:               fhvmMetadata.LatestPodNumber,
+				LatestVerifiedPodNumber:       fhvmMetadata.LatestVerifiedPodNumber,
+				FinalityPodNumber:             upperBondPod,
+			}
+
+			byteFhvmMetadata := k.cdc.MustMarshal(&updateFhvmMetadata)
+			fhvmMetadataDB.Set(fhvmMetadataKey, byteFhvmMetadata)
+			// update the pod-finalized-count
+			figureDBStore := prefix.NewStore(storeAdapter, types.KeyPrefix(types.FiguresDBPath))
+			podFinalizedCountKey := fmt.Sprintf("zkFHE-pod-finalized-count__%s", stationId)
+			podNumberString := strconv.FormatUint(upperBondPod, 10)
+			figureDBStore.Set([]byte(podFinalizedCountKey), []byte(podNumberString))
+
+			return &types.MsgIntegrityCheckResponse{
+				Status: true,
+			}, nil
+		} else {
+			return nil, status.Error(codes.InvalidArgument, "invalid blobRef passed")
+		}
 	} else {
 		return nil, status.Error(codes.InvalidArgument, "invalid DA provider")
 	}
